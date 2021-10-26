57d29b7813035df0fb0d037f978d1f34fde696e7
vilipche <33267207+vilipche@users.noreply.github.com>
2021-10-26 22:40:40 +0200

    fixing errors

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,5 @@
-
-# # # $1 -> absolute path of the tpc-ds benchmark folder
-# # # $2 -> size of the data (GB)
+# # # # $1 -> absolute path of the tpc-ds benchmark folder
+# # # # $2 -> size of the data (GB)
 
 PATH_BENCH="$1"
 dir_pwd=$(pwd)
@@ -8,28 +7,28 @@ cd "$1/tools"
 chmod 777 dsqgen dsdgen
 
 
-# # create the queries
-# # if you run this, the script will overwrite the queries in the repository -> queries/queries_i
-# echo "Generating $2 GB of queries. The queries are in the /queries/queries_$2 folder"
-# # in order to avoid error:
-# echo "define _END = "";" >> /queries_templates/netezza.tpl
+# # # create the queries
+# # # if you run this, the script will overwrite the queries in the repository -> queries/queries_i
+# # echo "Generating $2 GB of queries. The queries are in the /queries/queries_$2 folder"
+# # # in order to avoid error:
+echo 'define _END = ";"' >> "$1/query_templates/netezza.tpl"
 
-# for i in {1..99}; do
-#     ./dsqgen -template "query${i}.tpl" -directory ../query_templates/ -dialect netezza -scale $2 
-#     mv query_0.sql "$dir_pwd/queries/queries_${2}/query_${i}.sql"
-# done
+for i in {1..99}; do
+    ./dsqgen -template "query${i}.tpl" -directory ../query_templates/ -dialect netezza -scale $2 
+    mv query_0.sql "$dir_pwd/queries/queries_${2}/query_${i}.sql"
+done
 
-# # create the values
-# echo "Generating $2 GB of data"
+# # # create the values
+echo "Generating $2 GB of data"
 `"$PATH_BENCH/tools/dsdgen" -dir /tmp –scale $2 -force Y` # only works like this lol
 
-# # create the database
-sudo -u postgres psql -c "create database tpcds;"
+# # # create the database
+sudo -u postgres psql -c "create database tpcds"
 sudo -u postgres psql tpcds -f 'tpcds.sql'
-sudo -u postgres psql tpcds -f "SET CLIENT_ENCODING TO 'latin1'"
+sudo -u postgres psql tpcds -c "SET CLIENT_ENCODING TO 'latin1'" #bugs
 
 
-# load the values
+# # load the values
 cd /tmp
 for i in `ls *.dat`; 
 do table=${i/.dat/}; 
@@ -40,15 +39,13 @@ sudo -u postgres psql tpcds -c "\\copy $table FROM '/tmp/$i' CSV DELIMITER '|'";
 
 
 # in case customer does not load try this:
-# sudo -u postgres psql tpcds -f "SET CLIENT_ENCODING TO 'latin1'"
-# sudo -u postgres psql tpcds -c "\\copy $table FROM '/tmp/$i' CSV DELIMITER '|'";
+sudo -u postgres psql tpcds -c "SET CLIENT_ENCODING TO 'latin1;'"
+sudo -u postgres psql tpcds -c "\\copy customer FROM '/tmp/customer.dat' CSV DELIMITER '|'";
 # in case this still doesn't work open the databse from terminal and add both commands
 
 # run the queries
 
-cd $dir_pwd
+cd "${dir_pwd}"
 # please make sure that you change the database, username and password.
 # this example creates a database called "tpcds"
-
 python3 query_executor.py
-
