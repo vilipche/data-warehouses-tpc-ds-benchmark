1f67cc87f0df242d2625f8a767586da87e14d0c8
vilipche <33267207+vilipche@users.noreply.github.com>
2021-10-31 15:21:20 +0100

    minor fixes benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -9,12 +9,12 @@ chmod 777 dsqgen dsdgen
 
 # # # create the queries
 # # # if you run this, the script will overwrite the queries in the repository -> queries/queries_i
-# # echo "Generating $2 GB of queries. The queries are in the /queries/queries_$2 folder"
+echo "Generating $2 GB of queries. The queries are in the /queries/queries_$2 folder"
 # # # in order to avoid error:
-echo 'define _END = ";"' >> "$1/query_templates/netezza.tpl"
+echo 'define _END = "";' >> "$1/query_templates/netezza.tpl"
 
 for i in {1..99}; do
-    ./dsqgen -template "query${i}.tpl" -directory ../query_templates/ -dialect netezza -scale $2 
+    ./dsqgen -template "query${i}.tpl" -directory ../query_templates -dialect netezza -scale $2 
     mv query_0.sql "$dir_pwd/queries/queries_${2}/query_${i}.sql"
 done
 
@@ -23,13 +23,16 @@ echo "Generating $2 GB of data"
 `"$PATH_BENCH/tools/dsdgen" -dir /tmp –scale $2 -force Y` # only works like this lol
 
 # # # create the database
+echo 'Creating the database'
+
 sudo -u postgres psql -c "create database tpcds"
 sudo -u postgres psql tpcds -f 'tpcds.sql'
-sudo -u postgres psql tpcds -f 'tpcds_ri.sql'
-sudo -u postgres psql tpcds -c "SET CLIENT_ENCODING TO 'latin1'" #bugs
+sudo -u postgres psql tpcds -c "SET CLIENT_ENCODING TO 'latin1'" # may not work. see the fix at the end.
 
 
 # # load the values
+echo 'Loading the values'
+
 cd /tmp
 for i in `ls *.dat`; 
 do table=${i/.dat/}; 
@@ -39,12 +42,17 @@ sudo -u postgres psql tpcds -q -c "TRUNCATE $table";
 sudo -u postgres psql tpcds -c "\\copy $table FROM '/tmp/$i' CSV DELIMITER '|'"; done
 
 
+# # # adding the foreign keys after loading the data
+sudo -u postgres psql tpcds -f 'tpcds_ri.sql'
+
+
 # in case customer does not load try this:
 #sudo -u postgres psql tpcds -c "SET CLIENT_ENCODING TO 'latin1;'"
 #sudo -u postgres psql tpcds -c "\\copy customer FROM '/tmp/customer.dat' CSV DELIMITER '|'";
 # in case this still doesn't work open the databse from terminal and add both commands
 
 # run the queries
+echo 'Running the queries' 
 
 cd "${dir_pwd}"
 # please make sure that you change the database, username and password.
